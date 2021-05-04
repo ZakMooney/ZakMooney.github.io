9d36be09c112c8708b7ee96e011e9eaf47c5ae4f
Zak <zak@moonma.nz>
2021-05-04 01:09:06 +0100

    moon

--- src/home/index.js
+++ src/home/index.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import moon from '../moon.svg';
+import '../App.css';
 
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="moon-con">
+            <img src={moon} className="moon" alt="logo" />
+            <div className="moonpulse"></div>
+        </div>
+        <h1>MOONZ</h1>
     </div>
   );
 }
